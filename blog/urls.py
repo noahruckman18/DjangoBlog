81d48f9a772856a499e90c46b47a52cc51f103b7
Noah <nkillianruckman@ivytech.edu>
2023-05-07 17:07:03 -0400

    Extensions

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
-from django.urls import re_path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
-    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit')
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    re_path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    re_path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    re_path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    re_path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.urls import path, include, re_path
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'', include('blog.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
